2238d406d3b4ddc3b62cff48953e6ecbe67fc7b5
Moon Antonio <pinchodev91@gmail.com>
2018-07-16 19:34:55 +0200

    Creado Player.cs para controlar cliente

---
+++ UNet Study Project/Assets/UNet/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// Player.cs (16/07/2018)														\\
+// Autor: Antonio Mateo (.\Moon Antonio) 	antoniomt.moon@gmail.com			\\
+// Descripcion:		Controller del jugador/Cliente.								\\
+// Fecha Mod:		16/07/2018													\\
+// Ultima Mod:		Version Inicial												\\
+//******************************************************************************\\
+
+#region Librerias
+using UnityEngine;
+#endregion
+
+namespace MoonAntonio
+{
+	/// <summary>
+	/// <para>Controller del jugador/Cliente.</para>
+	/// </summary>
+	public class Player : MonoBehaviour 
+	{
+
+	}
+}
